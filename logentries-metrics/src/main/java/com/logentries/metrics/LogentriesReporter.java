584962601672b8171a98e35e42d8d1c564a96103
Chris Mowforth <chris@mowforth.com>
2013-10-13 05:06:25 +0100

    add logentries-metrics skeleton

---
+++ logentries-metrics/src/main/java/com/logentries/metrics/LogentriesReporter.java
@@ -0,0 +1,30 @@
+package com.logentries.metrics;
+
+import com.codahale.metrics.*;
+import com.logentries.core.Client;
+import com.logentries.core.IClient;
+
+import java.util.SortedMap;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ */
+public class LogentriesReporter extends ScheduledReporter {
+
+    private final IClient client;
+
+    private LogentriesReporter(MetricRegistry registry,
+                               String logToken,
+                               TimeUnit rateUnit,
+                               TimeUnit durationUnit,
+                               MetricFilter filter) {
+        super(registry, "logentries-reporter", filter, rateUnit, durationUnit);
+        this.client = Client.Builder.get().withToken("").build();
+    }
+    @Override
+    public void report(SortedMap<String, Gauge> gauges, SortedMap<String, Counter> counters, SortedMap<String, Histogram> histograms, SortedMap<String, Meter> meters, SortedMap<String, Timer> timers) {
+        // TODO
+    }
+
+}

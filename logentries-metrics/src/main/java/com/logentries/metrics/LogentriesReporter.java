81ae281e6fbc94632521b5edde7e8eb7378a5586
Chris Mowforth <chris@mowforth.com>
2014-01-09 00:15:09 +0000

    everything builds

--- logentries-metrics/src/main/java/com/logentries/metrics/LogentriesReporter.java
+++ logentries-metrics/src/main/java/com/logentries/metrics/LogentriesReporter.java
@@ -1,8 +1,7 @@
 package com.logentries.metrics;
 
 import com.codahale.metrics.*;
-import com.logentries.core.Client;
-import com.logentries.core.LogentriesClientI;
+import com.logentries.core.LogentriesClient;
 
 import java.util.SortedMap;
 import java.util.concurrent.TimeUnit;
@@ -12,16 +11,18 @@ import java.util.concurrent.TimeUnit;
  */
 public class LogentriesReporter extends ScheduledReporter {
 
-    private final LogentriesClientI client;
+    private final LogentriesClient client;
 
-    private LogentriesReporter(MetricRegistry registry,
-                               String logToken,
-                               TimeUnit rateUnit,
-                               TimeUnit durationUnit,
-                               MetricFilter filter) {
+    private LogentriesReporter(
+            LogentriesClient client,
+            MetricRegistry registry,
+            TimeUnit rateUnit,
+            TimeUnit durationUnit,
+            MetricFilter filter) {
         super(registry, "logentries-reporter", filter, rateUnit, durationUnit);
-        this.client = Client.Builder.get().withToken("").build();
+        this.client = client;
     }
+
     @Override
     public void report(SortedMap<String, Gauge> gauges, SortedMap<String, Counter> counters, SortedMap<String, Histogram> histograms, SortedMap<String, Meter> meters, SortedMap<String, Timer> timers) {
         // TODO
---
+++ logentries-metrics/src/test/java/com/logentries/metrics/LogentriesReporterTest.java
@@ -0,0 +1,7 @@
+package com.logentries.metrics;
+
+/**
+ * CHANGEME
+ */
+public class LogentriesReporterTest {
+}
